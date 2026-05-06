e2899a038a621485c2cc1c2c9241d636e78ac5d4
RoneOleh <olegkroh14@gmail.com>
2026-05-07 00:21:09 +0300

    onlyproblems

--- internal/app/organization_service.go
+++ internal/app/organization_service.go
@@ -12,6 +12,7 @@ type organizationService struct {
 }
 type OrganizationService interface {
 	Save(o domain.Organization) (domain.Organization, error)
+	FindList(uId uint64) ([]domain.Organization, error)
 }
 
 func NewOrganizationService(or database.OrganizationRepository) OrganizationService {
@@ -24,8 +25,18 @@ func (s organizationService) Save(o domain.Organization) (domain.Organization, e
 	org, err := s.orgRepo.Save(o)
 	if err != nil {
 		log.Printf("organizationService.Save(s.orgRepo.Save): %s", err)
-
+		return domain.Organization{}, err
 	}
+
 	return org, nil
 }
 
+func (s organizationService) FindList(uId uint64) ([]domain.Organization, error) {
+	orgs, err := s.orgRepo.FindList(uId)
+	if err != nil {
+		log.Printf("organizationService.FindList(s.orgRepo.FindList): %s", err)
+		return nil, err
+	}
+
+	return orgs, nil
+}
