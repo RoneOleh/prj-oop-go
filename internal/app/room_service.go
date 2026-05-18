437b85f26a4f8ca49a102d02e04509dd7d9eaf12
RoneOleh <olegkroh14@gmail.com>
2026-05-18 11:00:27 +0300

    ADD:bugs

--- internal/app/room_service.go
+++ internal/app/room_service.go
@@ -6,23 +6,41 @@ type roomService struct {
 	roomRepo database.RoomRepository
 }
 type RoomService interface {
+	Save(room  domian.Room)(domain.Room,eror)
+    Update(room  domian.Room)(domain.Room,eror)
+	Delete(id uint64) error
 }
 
 type NewRoomService (roomRepo database.RoomRepository)RoomService{
-    rm, err := s.orgRepo.Save(o)
+    rm, err := s.orgRepo.Save(room)
 	if err != nil {
-		log.Printf("organizationService.Save(s.orgRepo.Save): %s", err)
+		log.Printf("roomService.Save(s.roomRepo.Save): %s", err)
 		return domain.Organization{}, err
 	}
 
 	return rm, nil
 }
 func (s roomService)Save(room  domian.Room)(domain.Room,eror){
-    rm, err := s.orgRepo.Save(o)
+    rm, err := s.orgRepo.Save(room)
 	if err != nil {
-		log.Printf("organizationService.Save(s.orgRepo.Save): %s", err)
-		return domain.Organization{}, err
+		log.Printf("roomService.Save(s.roomRepo.Save): %s", err)
+		return domain.Room{}, err
 	}
 
 	return rm, nil
+}
+func (s roomService)Update(room  domian.Room)(domain.Room,eror){
+    rm, err := s.orgRepo.Update(room)
+	if err != nil {
+		log.Printf("roomService.Save(s.roomRepo.Save): %s", err)
+		return domain.Room{}, err
+	}
+
+	return rm, nil
+func (s roomService) Delete(id uint64) error {
+	err := s.orgRepo.Delete(id)
+	if err != nil {
+		log.Printf("roomService.Delete(s.roomRepo.Delete): %s", err)
+		return err
+	}	
 }
