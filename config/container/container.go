d7f035896d630c9a98c35ce52872fd546fc75166
olegkroh14@gmail.com <0532box@gmail.com>
2026-06-08 15:36:41 +0300

    1233

--- config/container/container.go
+++ config/container/container.go
@@ -62,7 +62,7 @@ func New(conf config.Configuration) Container {
 	roomService := app.NewRoomService(roomRepository)
 	deviceService := app.NewDeviceService(deviceRepository)
 	measurementService := app.NewMeasurementService(measurementRepository)
-    eventService := app.NewEventService(eventRepository)
+    eventService := app.NewEventService(eventRepository,deviceRepository)
 
 	authController := controllers.NewAuthController(authService, userService)
 	userController := controllers.NewUserController(userService, authService)
